b9ef9eaa0d14a08a4c21d975ec784cb50da7790f
CesarRicardo95 <ces.237ricardo@gmail.com>
2026-04-29 13:39:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ const estudiantes = {
         "Bianca Ashley Ortega Chamba",
         "Amir Jeremy Alejandro Salcedo",
         "Jordan Sebastian Lino Dominguez"
+    ],
+
+    "Clase 2": [
         "Fausto David Rodriguez Pozo",
         "Sheyla Valeska Pilligua Pita",
         "Lissette Susana Solano Quinde",
         "David Gomez Solano",
-    ],
-
-    "Clase 2": [
         "Nicolas Michael Villon Aquino",
         "Doménica Yaritza Rodríguez Suárez",
         "Jesus Mejillones Tigrero",
@@ -26,7 +26,10 @@ const estudiantes = {
         "Karla Mayle Tomalá Cruz",
         "Mirellys Lilibeth Ricardo Rodriguez",
         "Ashley Samantha Zambrano Chenche",
-        "Lisbeth Gonzalez Ortiz",
+        "Lisbeth Gonzalez Ortiz"
+    ],
+
+    "Clase 3": [
         "Dayana Valentina Muñiz Quimi",
         "Daniel Fernando Candelario Panchana",
         "Ivanna Valentina Carvajal Orrala",
@@ -38,13 +41,13 @@ const estudiantes = {
         "Jorge Wladimir Hernández Guevara",
         "Cristhel Mirelly Escalante Medina",
         "Kevin Josué Limones Muñoz",
-        "Scarlette Valeshka Lino Domínguez",
-        "Justin Javier Roca Tomala",
-        "Alexis Ariel López Rosales",
+        "Scarlette Valeshka Lino Domínguez"
     ],
 
-    "Clase 3": [
-       "Aylin Elizabeth Holguin Baque",
+    "Clase 4": [
+        "Justin Javier Roca Tomala",
+        "Alexis Ariel López Rosales",
+        "Aylin Elizabeth Holguin Baque",
         "Anderson Jahir Zambrano Chenche",
         "Felix Gabriel Muñiz Quimi",
         "Maria Fernanda Gonzabay Palma",
@@ -57,18 +60,14 @@ const estudiantes = {
         "Samuel Andrés Rodríguez Pozo",
         "Adrian Joel Rodriguez Suarez",
         "Niurka Rivera Muñoz",
-    ],
-
-    "Clase 4": [
-    
         "Fernanda Nicole Ricardo Orrala",
         "Danna Gomez Lindao",
         "Joselyn Jimena Muñiz Quimi",
         "Valeska Milena Escalante Medina",
         "Isabella Valeria Guzñay Gomez",
         "Natasha Isabel Bernabé Rivera",
-        "Ana Belen Rosales Sanchez",
-    ],
+        "Ana Belen Rosales Sanchez"
+    ]
 };
 
 document.addEventListener("DOMContentLoaded", cargarLista);
@@ -117,4 +116,5 @@ function guardarAsistencia() {
     localStorage.setItem("asistencia_" + clase, JSON.stringify(registros));
 
     alert("Asistencia guardada correctamente ✅");
+}
 }
